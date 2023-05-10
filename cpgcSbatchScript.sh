e62274baf6f5c8858d9ff979a9868ca5cb53d1d5
Akshar Chavan <aksharc22@gmail.com>
2023-05-10 17:40:45 -0400

    Update cpgcSbatchScript.sh
    updated the name of executable file

--- cpgcSbatchScript.sh
+++ cpgcSbatchScript.sh
@@ -37,7 +37,7 @@ do
         	for exp in 1 #2 3 4 5 6 7 8 9 10
         	do
 			#echo "$node,$density,$exp" >> Grid_sequential_CPA_results.csv
-            		./sequential  $node $density $exp >> Grid_sequential_CPA_results.csv
+            		./cpgc  $node $density $exp >> Grid_sequential_CPA_results.csv
             		echo $node $density $exp
         	done
     	done
