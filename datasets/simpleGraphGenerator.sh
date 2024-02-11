e016b204715699eeda1636ae2f14faea672b4116
Akshar Chavan <aksharc22@gmail.com>
2024-02-10 19:29:00 -0500

    Update simpleGraphGenerator.sh
    updated nodes and density list.

--- datasets/simpleGraphGenerator.sh
+++ datasets/simpleGraphGenerator.sh
@@ -2,9 +2,9 @@
 #cd /your current directory/Graph_Compression/datasets/
 #echo "nodes,density,experimentNo"
 
-for node in  16384 #32 64 128 256 512 1024 2048 4096 8192
+for node in   32 64 128 256 512 1024 2048 4096 8192 16384 32768
 do
-    for density in  90 95 98 #80 85
+    for density in  80 85 90 95 98
     do
 	for exp in  1 2 3 4 5 6 7 8 9 10
 	do
