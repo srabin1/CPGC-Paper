1b39aa69bca2c8b017ae6c5ca890381e7b71c9ed
Akshar Chavan <aksharc22@gmail.com>
2024-05-21 10:16:38 -0400

    Update simpleGraphGenerator.sh
    removed ids

--- datasets/simpleGraphGenerator.sh
+++ datasets/simpleGraphGenerator.sh
@@ -1,5 +1,3 @@
-#cd /wsu/home/gw/gw45/gw4590/Graph_Compression/dataset1/
-#cd /your current directory/Graph_Compression/datasets/
 #echo "nodes,density,experimentNo"
 
 for node in   32 64 128 256 512 1024 2048 4096 8192 16384 32768
