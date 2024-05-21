9e16061e6b5f49aabebb2ed6c017cd1b2b6441f7
Akshar Chavan <aksharc22@gmail.com>
2024-05-21 11:37:55 -0400

    Update cpgcbatchScript.sh

--- cpgcbatchScript.sh
+++ cpgcbatchScript.sh
@@ -1,27 +1,5 @@
 #!/bin/bash
-# Job name
-#SBATCH --job-name parallel_random_CPGC
-# Submit to the primary QoS
-#SBATCH -q primary
-# Request one node
-#SBATCH -N 1
-# Request entire node - the job allocation can not share nodes with other running jobs
-#SBATCH --exclusive
-# Total number of cores
-#SBATCH -n 1
-# Request memory
-#SBATCH --mem=250G
-# Mail when the job begins, ends, fails, requeues
-#SBATCH --mail-type=ALL
-# Where to send email alerts
-#SBATCH --mail-user=gw4590@wayne.edu
-# Create an output file that will be output_<jobid>.out
-#SBATCH -o output_%j.out
-# Create an error file that will be error_<jobid>.out
-#SBATCH -e errors_%j.err
-# Set maximum time limit
-#SBATCH -t 96:00:00
-#cd /wsu/home/gw/gw45/gw4590/Graph_Compression
+
 echo nodes,density,exp,delta,compressionRatio,executionTime > cpgc_results.csv 
 echo nodes,total_nodes,density,exp,delta,maximumFlow,run_time,total_run_time > bipartite_dinics_results.csv
 echo nodes,total_nodes,density,exp,delta,maximumFlow,run_time,total_run_time > tripartite_dinics_results.csv
