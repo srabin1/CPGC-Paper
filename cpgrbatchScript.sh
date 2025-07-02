e4fb27ca84bff3c404eaf996491c553ea8343c25
Sanaz Rabinia <47728478+srabin1@users.noreply.github.com>
2025-07-02 10:50:23 -0500

    Update cpgrbatchScript.sh
    replaced all cpgc with cpgr

--- cpgrbatchScript.sh
+++ cpgrbatchScript.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo nodes,density,exp,delta,compressionRatio,executionTime > cpgc_results.csv 
+echo nodes,density,exp,delta,compressionRatio,executionTime > cpgr_results.csv 
 echo nodes,total_nodes,density,exp,delta,maximumFlow,run_time,total_run_time > bipartite_dinics_results.csv
 echo nodes,total_nodes,density,exp,delta,maximumFlow,run_time,total_run_time > tripartite_dinics_results.csv
-algorithm="cpgc"
+algorithm="cpgr"
 for node in  32 64 128 256 512 1024 2048 4096 8192 16384 32768 
 do
     	for density in 80 85 90 95 98 
@@ -12,9 +12,9 @@ do
         	do
 	 		for delta in 0.5 0.6 0.7 0.8 0.9 1
     			do
-       				./cpgc  $node $density $exp $delta>> cpgc_results.csv
+       				./cpgr  $node $density $exp $delta>> cpgr_results.csv
             			echo $node $density $exp
-				#fileName="datasets/cpgc_tripartite_graph_${node}_${density}_${exp}.mtx"
+				#fileName="datasets/cpgr_tripartite_graph_${node}_${density}_${exp}.mtx"
 				./dinics_bi  $node $density $exp $delta >> bipartite_dinics_results.csv	
 				./dinics_tri  $node $density $exp $delta $algorithm >> tripartite_dinics_results.csv
 				#rm "$fileName"
