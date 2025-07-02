7b4ed8d3ec7b68044d9af65981a77175a1248459
Sanaz Rabinia <47728478+srabin1@users.noreply.github.com>
2025-07-02 10:50:54 -0500

    Update batchExecution.bash
    replaced all cpgc with cpgr

--- batchExecution.bash
+++ batchExecution.bash
@@ -2,21 +2,21 @@
 
 # Initialize CSV files
 for opt_level in O0 O1 O2 O3 O4; do
-    echo "nodes,density,exp,delta,compressionRatio,executionTime" > "cpgc_results_${opt_level}.csv"
+    echo "nodes,density,exp,delta,compressionRatio,executionTime" > "cpgr_results_${opt_level}.csv"
     echo "nodes,density,exp,delta,compressionRatio,executionTime" > "fm_results_${opt_level}.csv"
 
     echo "nodes,total_nodes,density,exp,delta,maximumFlow,run_time,total_run_time" > "bipartite_dinics_results_${opt_level}.csv"
     echo "nodes,total_nodes,density,exp,delta,maximumFlow,run_time,total_run_time" > "tripartite_dinics_results_${opt_level}.csv"
 done
 
-algorithm="cpgc"
+algorithm="cpgr"
 for node in 32 64 128 256 512 1024 2048 4096 8192 16384 32768; do
     for density in 80 85 90 95 98; do
         for exp in {1..10}; do
             for delta in 0.5 0.6 0.7 0.8 0.9 1; do
-                # Run cpgc algorithms
+                # Run cpgr algorithms
                 for opt_level in O0 O1 O2 O3 O4; do
-                    ./cpgc_$opt_level "$node" "$density" "$exp" "$delta" >> "cpgc_results_${opt_level}.csv"
+                    ./cpgr_$opt_level "$node" "$density" "$exp" "$delta" >> "cpgr_results_${opt_level}.csv"
                 done
 
                 # Conditionally run fm algorithm for small graphs
